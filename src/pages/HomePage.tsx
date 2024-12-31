bc0a4202676e880fc30be662f80ba55f74bd6011
devpoi <smboy86@naver.com>
2024-12-31 22:48:09 +0900

    add - 알럿 다이얼로그

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,6 +2,17 @@ import React from "react";
 import ToggleTheme from "@/components/ToggleTheme";
 import { useTranslation } from "react-i18next";
 import LangToggle from "@/components/LangToggle";
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
 
 export default function HomePage() {
     const { t } = useTranslation();
@@ -12,6 +23,23 @@ export default function HomePage() {
                 <h1 className="text-4xl font-bold">{t("title")}</h1>
                 <LangToggle />
                 <ToggleTheme />
+                <div className="">dfsdfsf ddd</div>
+                <AlertDialog>
+                    <AlertDialogTrigger>Open</AlertDialogTrigger>
+                    <AlertDialogContent>
+                        <AlertDialogHeader>
+                            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                            <AlertDialogDescription>
+                                This action cannot be undone. This will permanently delete your
+                                account and remove your data from our servers.
+                            </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                            <AlertDialogCancel>Cancel</AlertDialogCancel>
+                            <AlertDialogAction>Continue</AlertDialogAction>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialog>
             </div>
         </>
     );
